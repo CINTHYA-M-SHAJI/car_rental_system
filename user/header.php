da7ae858e08e525b505294e83ecd6a12b0dfbf1b
CINTHYA M SHAJI <CINTHYA-M-SHAJI>
2022-08-02 18:06:21 +0530

    user_header

---
+++ user/header.php
@@ -0,0 +1,50 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Car Rental System</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,500,600,700,800&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="css/open-iconic-bootstrap.min.css">
+    <link rel="stylesheet" href="css/animate.css">
+    
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/magnific-popup.css">
+
+    <link rel="stylesheet" href="css/aos.css">
+
+    <link rel="stylesheet" href="css/ionicons.min.css">
+
+    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="css/jquery.timepicker.css">
+
+    
+    <link rel="stylesheet" href="css/flaticon.css">
+    <link rel="stylesheet" href="css/icomoon.css">
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+    
+	  <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
+	    <div class="container">
+	      <a class="navbar-brand"> <span>USER NAME: <input type="name" readonly value="<?php echo $_SESSION['name'];?>"></a>
+	     
+
+	      <div class="collapse navbar-collapse" id="ftco-nav">
+	        <ul class="navbar-nav ml-auto">
+	          <li class="nav-item"><a href="userindex.php" class="nav-link">Home</a></li>
+        
+	          <!-- 	<li class="nav-item"><a href="pricing.html" class="nav-link">Pricing</a></li> -->
+	          <li class="nav-item"><a href="view_car.php" class="nav-link">Cars</a></li>
+            <li class="nav-item"><a href="profile.php" class="nav-link">Profile</a></li>
+	          <li class="nav-item"><a href="view_booking.php" class="nav-link">Booking</a></li>
+	          <li class="nav-item"><a href="logout.php" class="nav-link">Logout</a></li>
+	        </ul>
+	      </div>
+	    </div>
+	  </nav>
+    <!-- END nav -->
+    </html>
